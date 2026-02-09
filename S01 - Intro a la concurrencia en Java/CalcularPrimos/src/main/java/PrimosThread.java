da22fc196d17f0c334d3980bf473b6c7a286251e
Blockky <blockygh6@hotmail.com>
2026-02-09 13:02:48 +0100

    Añadido CalcularPrimos de la S01

---
+++ S01 - Intro a la concurrencia en Java/CalcularPrimos/src/main/java/PrimosThread.java
@@ -0,0 +1,37 @@
+/**
+ * @author Blocky
+ */
+public class PrimosThread extends Thread {
+    //Atributos
+    private int x, y, n=0;
+    
+    //Constructor
+    public PrimosThread(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    //Métodos
+    private boolean esPrimo(int n) {
+        int raiz = (int) Math.sqrt((double) n);
+        boolean primo = true;
+        int i = 2;
+        while (primo && i <= raiz) {
+            if (n % i == 0) 
+                primo = false;
+            i++;
+        }
+        return primo;
+    }
+    
+    public void run() {
+        for (int i = x; i <= y; i++) {
+            if (esPrimo(i))
+                n++;
+        }
+    }
+    
+    public int cuantos() {
+        return n;
+    }
+}
