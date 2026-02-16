3c22e484db3979412f0900ff558db4dc7eb56e3e
Blockky <blockygh6@hotmail.com>
2026-02-16 12:15:36 +0100

    Pintor de la S1

---
+++ S01 - Intro a la concurrencia en Java/VentanaColores/src/main/java/Pintor.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import javax.swing.JButton;
+
+/**
+ * @author Blocky
+ */
+public class Pintor extends Thread {
+    //Atributos
+    JButton b;
+    Color paleta[] = new Color[5];
+    
+    //Constructor
+    public Pintor(JButton b) {
+        this.b = b;
+        paleta[0] = Color.BLACK;
+        paleta[1] = Color.RED;
+        paleta[2] = Color.GREEN;
+        paleta[3] = Color.BLUE;
+        paleta[4] = Color.YELLOW;
+    }
+    
+    //Métodos
+    public void run() {
+        while (true) {
+            int i = (int)(5. * Math.random());
+            b.setBackground(paleta[i]);
+        }
+    }
+}
---
+++ S01 - Intro a la concurrencia en Java/VentanaColores/src/main/java/Ventana.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+
+/**
+ * @author Blocky
+ */
+public class Ventana extends javax.swing.JFrame {
+ 
+    public Ventana() {
+        initComponents();
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setTitle("Colores");
+        new Pintor(jButton1).start();
+        new Pintor(jButton2).start();
+        new Pintor(jButton3).start();
+        new Pintor(jButton4).start();
+    }
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(52, 52, 52)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(48, 48, 48))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(72, 72, 72)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 63, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(71, 71, 71))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(
+            new Runnable() {
+                public void run() {
+                    new Ventana();
+                }
+            }
+        );
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    // End of variables declaration//GEN-END:variables
+}
